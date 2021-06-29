208ffc0b84391db13f42141efd66a308070591c5
oluwafemi13 <36483191+oluwafemi13@users.noreply.github.com>
2021-06-30 00:36:00 +0100

    Desktop
    Updated side-hustle backend task on functions using php

--- SIDEHUSTLE/functions.php
+++ SIDEHUSTLE/functions.php
@@ -1,8 +1,27 @@
 <?PHP
 
+/**range of functions that takes two arguments */
 function range($start, $end){
-    
+        
+    $startToEnd = array($start, $end);
+    return $startToEnd;
 }
 
+echo range(30, 50);
+echo range(10, 70);
+
+
+/**sum function that takes an array of numbers and returns the sum of these numbers */
+
+function sum($numbers[10]){
+
+    
+    $entireSum =($numbers[0]+$numbers[1]+$numbers[2]+$numbers[3]+$numbers[4]+$numbers[5]+$numbers[6]+$numbers[7]+$numbers[8]+$numbers[9]);
+    
+    return $entireSum;
+}
+
+    
+    var_dump (sum(3,4,5,6,7,8,9,2,10,20));
 
 ?>
